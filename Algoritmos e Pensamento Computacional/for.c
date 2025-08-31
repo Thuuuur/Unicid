ce3e8365a8b0559e57b6a7067889036e79a10b36
Arthur Rodrigues Dantas <164256979+Thuuuur@users.noreply.github.com>
2025-08-31 18:51:08 -0300

    Add files via upload

---
+++ Algoritmos e Pensamento Computacional/for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//exercicio for
+int main(){
+
+    int x;
+
+    for (x=1; x<=10; x++)
+    printf("\nCurso de redes");
+    printf("\n");
+    system("PAUSE");
+
+    return 0;
+}
